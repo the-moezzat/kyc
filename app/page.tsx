27bc3a940bc2b8739e9e3042c7f5e871d7cf749a
Mohamed Ezzat <ezzat7014@gmail.com>
2024-01-20 03:14:43 +0200

    feat: easy navigate from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
-import { Button } from "@/components/ui/button";
 import Sidebar from "@/components/sidebar";
+import Link from "next/link";
+import {DeviceMobile, House} from "@phosphor-icons/react/dist/ssr";
 
  export default  function Page() {
   return (
     <div className={'grid grid-cols-[64px,1fr] h-screen max-w-screen-2xl mx-auto overflow-hidden  max-md:grid-cols-1 max-md:grid-rows-[64px,1fr] '}>
         <Sidebar/>
-        <div className={' overflow-auto p-8'}>
-            <Button variant={"default"} className={''}>Hello world</Button>
+        <div className={'overflow-auto p-8 flex items-center justify-center'}>
+            <div className={"flex gap-4 max-md:flex-col"}>
+            <Link href={"/mobile"} className={"border p-4 rounded-lg flex flex-col gap-2 items-center justify-center"}>
+                <DeviceMobile size={42} className={'text-blue-500'} />
+                <span className={'text-base text-gray-600 font-medium'}>
+                    Mobile app
+                </span>
+
+            </Link>
+            <Link href={"/home"} className={"border p-4 rounded-lg flex flex-col gap-2 items-center justify-center"}>
+                <House size={42} className={'text-green-500'} />
+                <span className={'text-base text-gray-600 font-medium'}>
+                  Dashboard
+                </span>
+
+            </Link>
+            </div>
         </div>
     </div>
   );
